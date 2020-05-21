b522a7d846b487c56847dacdbbfb6cd047073bd4
sotnii <sotnii@gmail.com>
2020-05-21 23:27:24 +0200

    bez testowania i wyjatkow

--- lab9/src/app.ts
+++ lab9/src/app.ts
@@ -10,6 +10,7 @@ store.addMeme(new MemClass(10, 'Gold', 100, 'https://i.redd.it/h7rplf9jt8y21.png
 store.addMeme(new MemClass(9, 'Platinium', 1100, 'http://www.quickmeme.com/img/90/90d3d6f6d527a64001b79f4e13bc61912842d4a5876d17c1f011ee519d69b469.jpg'));
 store.addMeme(new MemClass(8, 'Elite', 120, 'https://i.imgflip.com/30zz5g.jpg'));
 store.addMeme(new MemClass(7, 'Bronze', 700, 'http://www.gazetamiedzyszkolna.pl/wp-content/uploads/2016/02/macgyver-MEM-752x440.png'));
+store.addMeme(new MemClass(6, 'Sad', 999, 'https://i.pinimg.com/236x/6d/ee/bc/6deebc8a47ecfaf39cc8a8574a77599f.jpg'));
 
 const app = express();
 app.set('view engine', 'pug');
@@ -37,4 +38,9 @@ app.get('/meme/:memeId', (req, res) => {
     const meme = store.getMemeById(req.params.memeId);
     meme.changePrice(newPrice);
     res.render('meme', { title: 'Mem', message: 'Price history:', mem: meme });
- })
+ });
+
+app.use((req, res) => {
+    res.status(404);
+    res.render('404');
+})
--- lab9/src/server.ts
+++ lab9/src/server.ts
@@ -1,6 +1,6 @@
 import app from './app';
 
-const server = app.listen(1501, ()=> {
+const server = app.listen(1502, ()=> {
     console.log('App is running at ... in ...');
     console.log('Press Ctrl+c to stop');
 });
